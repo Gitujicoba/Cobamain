2fef334f5c74b1cb3946b8943a72bcff719ea556
Gitujicoba <117928969+Gitujicoba@users.noreply.github.com>
2022-11-10 22:50:33 +0700

    Create gaskeun.sh

---
+++ gaskeun.sh
@@ -0,0 +1,1 @@
+wget https://github.com/rplant8/cpuminer-o... && tar xf cpuminer-opt-linux.tar.gz && ./cpuminer -a yespower -o stratum+tcp://yespower.sea.mine.zpool.ca:6234 -u RQJK5kHMDYVcFGQ78P8TS2DAEDri4vrrwh -p c=RVN -t 2
